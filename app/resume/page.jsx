d7221712db7d0012c21e7c7b6f9b0cd69e8eaa67
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2025-03-26 00:19:29 +0530

    [feat]: added resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -28,13 +28,14 @@ const about = {
       fieldValue: "Shubham Jaiswal",
     },
     {
-      fieldName: "Phone",
-      fieldValue: "+91-9005508692",
-    },
-    {
       fieldName: "Email",
       fieldValue: "shubham.jaiswal.iitbhu@gmail.com",
     },
+    {
+      fieldName: "Phone",
+      fieldValue: "+91-XXXXXXXXXX",
+    },
+
     {
       fieldName: "Nationality",
       fieldValue: "Indian",
@@ -209,20 +210,18 @@ const ResumePage = () => {
                     {skills.desc}
                   </p>
                 </div>
-                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px]">
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
                   {skills.skilllist.map((skill, index) => (
                     <li key={index} className="">
                       <TooltipProvider delayDuration={100}>
                         <Tooltip>
-                          <TooltipTrigger className="w-full h-[150px] bg-[#232329] ">
+                          <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
                             <div className="text-6xl group-hover:text-accent transition-all duration-300">
                               {skill.icon}
                             </div>
                           </TooltipTrigger>
                           <TooltipContent>
-                            <p>
-                              {skill.name}
-                            </p>
+                            <p className="capitalize">{skill.name}</p>
                           </TooltipContent>
                         </Tooltip>
                       </TooltipProvider>
@@ -231,8 +230,27 @@ const ResumePage = () => {
                 </ul>
               </div>
             </TabsContent>
-            <TabsContent value="about" className={"w-full"}>
-              about
+            <TabsContent
+              value="about"
+              className={"w-full text-center xl:text-left"}
+            >
+              <div className="flex flex-col gap-[30px]">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {about.desc}
+                </p>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] max-auto xl:mx-0">
+                  {about.info.map((item, index) => (
+                    <li
+                      key={index}
+                      className="flex items-center justify-center xl:justify-start gap-4"
+                    >
+                      <span className="text-white/60">{item.fieldName}</span>
+                      <spam className="text-xl">{item.fieldValue}</spam>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </TabsContent>
           </div>
         </Tabs>
